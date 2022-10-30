0dda452fef0cdba96cb7342edbfbb331d5b7ea71
BlackFiber <115573303+BlackFiber@users.noreply.github.com>
2022-10-31 01:34:12 +0530

    Add files via upload

---
+++ FirstNPrimeNumber.java
@@ -0,0 +1,30 @@
+package college_progamme;
+import java.util.Scanner;
+
+
+public class FirstNPrimeNumber {
+    public static void main(String[] args){
+       
+            Scanner scan = new Scanner(System.in);
+            System.out.print("Enter the upper range : ");
+            int N = scan.nextInt();
+            int x = 2;
+        
+            for(int i = 0; i <= N-2; i++)
+            {
+                int count = 0;
+        
+                for(int j = 1; j <= x; j++)
+                    if(x%j == 0)
+                        count++;
+        
+                if(count == 2)
+                    System.out.print(x + " ");
+        
+                x++;
+            }
+        }
+
+    
+    
+}
